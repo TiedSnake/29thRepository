e97ecc0ab1d80b006be68b7b3aae12b678f1acb4
Marwan <marwang11@gmail.com>
2021-06-27 22:44:18 +0300

    4.18 Assisted Practice project on Queues in Java

---
+++ src/main/QueueExample.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ *
+ * @author m-w-n
+ */
+public class QueueExample
+{
+    public static void main(String[] args)
+    {
+        Queue<String> locationsQueue = new LinkedList<>();
+        locationsQueue.add("Kolkata");
+        locationsQueue.add("Patna");
+        locationsQueue.add("Delhi");
+        locationsQueue.add("Gurgaon");
+        locationsQueue.add("Noida");
+        System.out.println("Queue is: " + locationsQueue);
+        System.out.println("Head of Queue: " + locationsQueue.peek());
+        locationsQueue.remove();
+        System.out.println("After removing Head of Queue: " + locationsQueue);
+        System.out.println("Size of Queue: " + locationsQueue.size());
+        
+    }
+}
